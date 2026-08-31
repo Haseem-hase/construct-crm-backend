1499bd1a45b96478cdf8a9841e8f36a9fe6c05a4
haseem <hasehaseem04@gmail.com>
2026-08-31 16:59:30 +0300

    role validation file written for create role and update role shcmeas

---
+++ src/modules/roles/role.validation.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+
+export const createRoleSchema = z.object({
+    name: z
+        .string()
+        .trim()
+        .min(2, "Role name must be at least 2 characters.")
+        .max(50, "Role name must not exceed 50 characters."),
+
+    description: z
+        .string()
+        .trim()
+        .max(255, "Description must not exceed 255 characters.")
+        .optional(),
+
+    permissionIds: z
+        .array(z.string().uuid("Invalid permission ID."))
+        .optional()
+        .default([]),
+})
+
+export const updateRoleSchema = z.object({
+    name: z
+        .string()
+        .trim()
+        .min(2, "Role name must be at least 2 characters.")
+        .max(50, "Role name must not exceed 50 characters.")
+        .optional(),
+
+    description: z
+        .string()
+        .trim()
+        .max(255, "Description must not exceed 255 characters.")
+        .nullable()
+        .optional(),
+
+    permissionIds: z
+        .array(z.string().uuid("Invalid permission ID."))
+        .optional(),   
+})
+
+export const roleIdSchema = z.object({
+    id:z.string().uuid("Invalid role ID."),
+});
+
+export type CreateRoleInput = z.infer<typeof createRoleSchema>;
+export type UpdateRoleInput = z.infer<typeof updateRoleSchema>;

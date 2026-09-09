89e2d60ba913da60095ab002b4dee48e076c12a3
haseem <hasehaseem04@gmail.com>
2026-09-09 23:27:00 +0300

    customer validation file has been completed and all unwanted files like tes-db.js everything was deleted

--- test-db3.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const customers = await prisma.$queryRaw`SELECT id, name, "customerCode", "parentCustomerId" FROM "customers" WHERE "parentCustomerId" IS NOT NULL;`;
-  console.log(customers);
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());

7bd2e27e1c97abf7fc30352d2428eafc09d90cc7
haseem <hasehaseem04@gmail.com>
2026-09-15 21:30:57 +0300

    authporize middleware has been updated to accept module,action

--- src/modules/projects/project-customer.routes.ts
+++ src/modules/projects/project-customer.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import { Module, Action } from "@prisma/client";
+import { authorize } from "../../middlewares/authorize";
 import * as projectCustomerController from "./project-customer.controller";
 
 const router = Router();
 
-router.get("/:projectId/customers", projectCustomerController.getProjectCustomers);
-router.post("/:projectId/customers", projectCustomerController.createProjectCustomer);
-router.get("/:projectId/customers/:projectCustomerId", projectCustomerController.getProjectCustomer);
-router.patch("/:projectId/customers/:projectCustomerId", projectCustomerController.updateProjectCustomer);
-router.delete("/:projectId/customers/:projectCustomerId", projectCustomerController.deleteProjectCustomer);
+router.get("/:projectId/customers", authorize(Module.PROJECT, Action.VIEW), projectCustomerController.getProjectCustomers);
+router.post("/:projectId/customers", authorize(Module.PROJECT, Action.UPDATE), projectCustomerController.createProjectCustomer);
+router.get("/:projectId/customers/:projectCustomerId", authorize(Module.PROJECT, Action.VIEW), projectCustomerController.getProjectCustomer);
+router.patch("/:projectId/customers/:projectCustomerId", authorize(Module.PROJECT, Action.UPDATE), projectCustomerController.updateProjectCustomer);
+router.delete("/:projectId/customers/:projectCustomerId", authorize(Module.PROJECT, Action.UPDATE), projectCustomerController.deleteProjectCustomer);
 
 export default router;
--- src/modules/projects/project.routes.ts
+++ src/modules/projects/project.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
+import { Module, Action } from "@prisma/client";
+import { authorize } from "../../middlewares/authorize";
 import * as projectController from "./project.controller";
 import projectCustomerRoutes from "./project-customer.routes";
 
 const router = Router();
 
-router.post("/", projectController.createProject);
-router.get("/", projectController.getOrganizationProjects);
+router.post("/", authorize(Module.PROJECT, Action.CREATE), projectController.createProject);
+router.get("/", authorize(Module.PROJECT, Action.VIEW), projectController.getOrganizationProjects);
 
 // Mount ProjectCustomer sub-resource routes
 router.use("/", projectCustomerRoutes);
 
-router.get("/:projectId", projectController.getProjectById);
-router.patch("/:projectId", projectController.updateProject);
-router.delete("/:projectId", projectController.deleteProject);
+router.get("/:projectId", authorize(Module.PROJECT, Action.VIEW), projectController.getProjectById);
+router.patch("/:projectId", authorize(Module.PROJECT, Action.UPDATE), projectController.updateProject);
+router.delete("/:projectId", authorize(Module.PROJECT, Action.DELETE), projectController.deleteProject);
 
 export default router;

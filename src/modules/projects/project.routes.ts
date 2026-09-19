989064b4ad7f887f6c5fc32202d78adb0a1f6537
haseem <hasehaseem04@gmail.com>
2026-09-19 20:54:05 +0300

    project routes and project customer routes has been refactored for authenticte

--- src/modules/projects/project.routes.ts
+++ src/modules/projects/project.routes.ts
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import { Module, Action } from "@prisma/client";
+import { authenticate } from "../../middlewares/authenticate";
 import { authorize } from "../../middlewares/authorize";
 import * as projectController from "./project.controller";
 import projectCustomerRoutes from "../project-customer/project-customer.routes";
 
 const router = Router();
 
-router.post("/", authorize(Module.PROJECT, Action.CREATE), projectController.createProject);
-router.get("/", authorize(Module.PROJECT, Action.VIEW), projectController.getOrganizationProjects);
+router.post("/", authenticate, authorize(Module.PROJECT, Action.CREATE), projectController.createProject);
+router.get("/", authenticate, authorize(Module.PROJECT, Action.VIEW), projectController.getOrganizationProjects);
 
 // Mount ProjectCustomer sub-resource routes
 router.use("/", projectCustomerRoutes);
 
-router.get("/:projectId", authorize(Module.PROJECT, Action.VIEW), projectController.getProjectById);
-router.patch("/:projectId", authorize(Module.PROJECT, Action.UPDATE), projectController.updateProject);
-router.delete("/:projectId", authorize(Module.PROJECT, Action.DELETE), projectController.deleteProject);
+router.get("/:projectId", authenticate, authorize(Module.PROJECT, Action.VIEW), projectController.getProjectById);
+router.patch("/:projectId", authenticate, authorize(Module.PROJECT, Action.UPDATE), projectController.updateProject);
+router.delete("/:projectId", authenticate, authorize(Module.PROJECT, Action.DELETE), projectController.deleteProject);
 
 export default router;
